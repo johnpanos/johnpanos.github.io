6309c48d2587155010afbec9760641a0b3addd20
John Panos <johncpanos@gmail.com>
2018-07-23 21:21:29 -0700

    Added warning for codec

--- precache-manifest.03ac0dfbf10d2507c421b0c9e8bb8ba5.js
+++ precache-manifest.03ac0dfbf10d2507c421b0c9e8bb8ba5.js
@@ -8,11 +8,11 @@ self.__precacheManifest = [
     "url": "/js/chunk-vendors.7539dc50.js"
   },
   {
-    "revision": "710817713d1a996fcf5d",
-    "url": "/js/app.71081771.js"
+    "revision": "eb3db6a81383a173ed34",
+    "url": "/js/app.eb3db6a8.js"
   },
   {
-    "revision": "d35587e834993e0f15cb365a2de28c65",
+    "revision": "ebd9425fee06c5ad1798c6bc9111575e",
     "url": "/index.html"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/css/chunk-vendors.6aa8b497.css"
   },
   {
-    "revision": "710817713d1a996fcf5d",
-    "url": "/css/app.db085b8a.css"
+    "revision": "eb3db6a81383a173ed34",
+    "url": "/css/app.b3e69cf4.css"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aaf79c558095940adef4f66d8b3c411c.js"
+  "/precache-manifest.03ac0dfbf10d2507c421b0c9e8bb8ba5.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "panos-music"});
